b36643e35cf63a6a7a6efc5242f17df67c60d3ee
iWtbAbh <1483601725@qq.com>
2018-12-27 11:48:28 +0800

    add row

--- client/src/layouts/LayOut.jsx
+++ client/src/layouts/LayOut.jsx
@@ -1,12 +1,11 @@
 import React, { Component } from 'react'
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import Style from './LayOut.less'
 
 import Sidebar from './Sidebar.jsx'
 
 import Header from '../components/Content/Header/Header.jsx'
-import Row from '../components/UI/grid/row.jsx'
-import Col from '../components/UI/grid/Col.jsx'
+import {Row, Col} from '../components/UI/grid/index.jsx'
 import Avtar from '../components/UI/Avtar/Avtar.jsx'
 import dog from '../assets/dog.jpg'
 import Button from '../components/UI/Button/Button.jsx'
@@ -24,14 +23,16 @@ const LayOut = () => (
 
     <main className={Style.ContentContainer}>
       <Header></Header>
-      <div className={Style.Main}>
-        <Route exact path="/" component={Blog} />
-        <Route path="/about"  component={About} />
-        <Route path="/users" component={Users} />
-        <Route path="/todo" component={ToDo} />
-        <Route path="/todo_x" component={Todos} />
-      </div>
 
+      <div className={Style.Main}>
+        <Switch>
+          <Route exact path="/" component={Blog} />
+          <Route path="/about"  component={About} />
+          <Route path="/users" component={Users} />
+          <Route path="/todo" component={ToDo} />
+          <Route path="/todo_x" component={Todos} />
+        </Switch>
+      </div>
     </main>
   </div>
 )
