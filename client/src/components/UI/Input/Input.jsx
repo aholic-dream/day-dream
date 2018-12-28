0c16ffca7f82f13fbecebc27bb5dc7e1bcc4bafa
iWtbAbh <1483601725@qq.com>
2018-12-28 17:27:02 +0800

    fix input material

--- client/src/components/UI/Input/Input.jsx
+++ client/src/components/UI/Input/Input.jsx
@@ -2,21 +2,55 @@ import React from 'react'
 import propTypes from 'prop-types'
 import classNames from 'classnames'
 import Style from './input.less'
-class Input extends React.Component {
 
-  constructor() {
+class Input extends React.Component {
+  constructor () {
     super()
+    this.container = ''
     this.domInput = ''
+  }
+  saveContainer (node) {
+    this.container = node
   }
   saveInput (node) {
     this.domInput = node
   }
   blur () {
+    if (this.domInput.value === '') {
+      this.container.classList.remove(Style.isCompleted)
+      this.container.classList.remove(Style.isActive)
+    } else {
+      this.container.classList.remove(Style.isActive)
+    }
     this.domInput.blur()
   }
-  focus() {
+  focus () {
+    this.container.classList.add(Style.isActive)
+    this.container.classList.add(Style.isCompleted)
     this.domInput.focus()
   }
+  renderLabel (children) {
+    const { prefix, id, size = 'normal'} = this.props
+    console.log(prefix)
+    if (!('prefix' in this.props)) {
+      return children
+    }
+
+    const classes = classNames({
+      [Style['prefix']]: prefix,
+      [Style[`${size}-label`]]: size
+    })
+
+    return (
+      <div className={Style.container} ref={node => this.saveContainer(node)}>
+        <label htmlFor={id} className={classes}>
+          {prefix}
+        </label>
+        {children}
+      </div>
+    )
+  }
+
   render () {
     const props = this.props
     const {
@@ -27,17 +61,20 @@ class Input extends React.Component {
       ...others
     } = props
 
-    const classes = classNames(
-      Style['input'],
-      {
-        [`${Style[size]}`]: size
-      }
-    )
-    
-    return (
-      <label htmlFor={id}>
-        <input {...others} id={id} type={type} className={classes} ref={node => this.saveInput(node)}/>
-      </label>
+    const classes = classNames(Style['input'], {
+      [`${Style[size]}`]: size
+    })
+
+    return this.renderLabel(
+      <input
+        {...others}
+        id={id}
+        type={type}
+        className={classes}
+        onFocus={e => this.focus(e)}
+        onBlur={e => this.blur(e)}
+        ref={node => this.saveInput(node)}
+      />
     )
   }
 }
@@ -45,7 +82,7 @@ class Input extends React.Component {
 Input.propTypes = {
   placeholder: propTypes.string,
   type: propTypes.string,
-  size: propTypes.oneOf(['small', 'normal', 'bg']),
+  size: propTypes.oneOf(['small', 'normal', 'big'])
 }
 
 export default Input
